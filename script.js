eaa0c1ea172bc1d035b499052e8974fed54b5410
Rider <goejrider@gmail.com>
2022-09-27 10:47:14 -0500

    Test out some minor mechanics of Node and it's limitations within JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('Hello from serverland! 🦍');
+
+let hat = 'Top Hat! 🎩'
+
+console.log(`Look at my ${hat}`);
+
+let results =  9+10;
+
+console.log(`Nine plus ten is equal to ${results}`);
+
+let testing = Math.sqrt(9)
+
+console.log('Can javascript find square roots?', testing);
+
+console.log('There is so much fun stuff you can do on here! I can\'t wait for you to find it all out!')
+
